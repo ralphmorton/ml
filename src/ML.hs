410ec838c0ea710ea60f328320b33fbc886630f2
Ralph Morton <ralphmorton@gmail.com>
2016-02-25 09:11:58 +0200

    Make input type generic

--- src/ML.hs
+++ src/ML.hs
@@ -8,13 +8,10 @@ module ML where
 import ML.Nat
 import ML.Vec
 
-type family VectorFunc (n :: Nat) (c :: *) :: *
-type instance VectorFunc Z c = c
-type instance VectorFunc (S n) c = Double -> VectorFunc n c
+type family VectorFunc (n :: Nat) (a :: *) (c :: *) :: *
+type instance VectorFunc Z a c = c
+type instance VectorFunc (S n) a c = a -> VectorFunc n a c
 
-classification :: SNat n -> VectorFunc n c -> Vec Double n -> c
-classification SZ r _ = r
-classification (SS n) rf (x:-xs) = classification n (rf x) xs
-
-regression :: SNat n -> VectorFunc n Double -> Vec Double n -> Double
-regression = classification
+hypothesis :: SNat n -> VectorFunc n a c -> Vec a n -> c
+hypothesis SZ r _ = r
+hypothesis (SS n) rf (x:-xs) = hypothesis n (rf x) xs
