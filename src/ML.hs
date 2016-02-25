aaf9bce43a3963684f365d08ca92411426f4b48c
Ralph Morton <ralphmorton@gmail.com>
2016-02-25 15:50:58 +0200

    Simplify hypothesis to accept a cardinality-indexed list as input

--- src/ML.hs
+++ src/ML.hs
@@ -9,9 +9,7 @@ import ML.Nat
 import ML.Vec
 
 type family VectorFunc (n :: Nat) (a :: *) (c :: *) :: *
-type instance VectorFunc Z a c = c
-type instance VectorFunc (S n) a c = a -> VectorFunc n a c
+type instance VectorFunc n a c = Vec a n -> c
 
-hypothesis :: SNat n -> VectorFunc n a c -> Vec a n -> c
-hypothesis SZ r _ = r
-hypothesis (SS n) rf (x:-xs) = hypothesis n (rf x) xs
+hypothesis :: VectorFunc n a c -> Vec a n -> c
+hypothesis h ix = h ix
